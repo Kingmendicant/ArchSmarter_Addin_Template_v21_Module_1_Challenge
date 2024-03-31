f3604524234b72fed18cc0c4f3dbad58a7bb7cbc
The Mendicant Architect <145192336+Kingmendicant@users.noreply.github.com>
2024-03-31 02:53:14 -0500

    Update Module_01_Challenge.cs

--- ArchSmarter_Addin_Template_v21_Module_1_Challenge/Module_01_Challenge.cs
+++ ArchSmarter_Addin_Template_v21_Module_1_Challenge/Module_01_Challenge.cs
@@ -49,7 +49,7 @@ namespace ArchSmarter_Addin_Template_v21_Module_1_Challenge
             // Loop through the number 1 to the number variable
             // Create a level for each number
 
-            for (int i = 0; i <= numberVar1; i++)
+            for (int i = 1; i <= numberVar1; i++)
             {
                 Level newLevel = Level.Create(doc, startElevation1);
                 newLevel.Name = "Level " + i.ToString() + " F.F.";
